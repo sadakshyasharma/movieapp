134f99bd91aa007a597d833c093c7b73958c44f0
Sadakshya Sharma <sadakshya.27s@gmail.com>
2023-01-25 11:23:13 +0530

    added default structure

--- movies/src/App.js
+++ movies/src/App.js
@@ -26,7 +26,7 @@ const App = () => {
       <h1> Movie World</h1>
 
       <div className="search">
-        <input
+        <input 
           placeholder="Search for Movies"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
